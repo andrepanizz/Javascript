0890ef477dd521e6f9d6277230008b408d40403b
Andre Panizza <andredevops7@gmail.com>
2023-11-12 18:29:40 -0300

    update

--- Projects_JS/scripts/calcController.js
+++ Projects_JS/scripts/calcController.js
@@ -5,15 +5,15 @@ class CalcController {
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
-        this.displayCalc = "0";
         this.currentDate;
         this.initialize();
       }
+
     initialize(){
         // Document object Model: or DOM
            
 
-            displayCalcEl.innerHTML = "123456";
+            
             dateEl.innerHTML = "01/05/2020";
             timeEl.innerHTML = "00:00";
 
@@ -22,12 +22,12 @@ class CalcController {
     /*Criando Getters and Setters: */
     get displayCalc(){
 
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
         
     }
 
     set displayCalc(valor) {
-        this._displayCalc = valor
+        this._displayCalcEl.innerHTML = valor;
     }
 
     get dataAtual(){
