680ce32c33a242b32b01dabeb611ec7ff3f8aa63
Andre Panizza <andredevops7@gmail.com>
2023-11-12 18:52:03 -0300

    update

---
+++ Projects_JS/scripts/calcController.js
@@ -0,0 +1,30 @@
+class CalcController {
+
+    constructor(){
+
+        this._displaycalcE1 = document.querySelector("");
+        this._dateE1 = document.querySelector("");
+        this._timeE1 = document.querySelector("");
+        this._currentDate;
+        this.initialize();
+    }
+
+    initialize(){
+
+
+    }
+
+// getters and setters:
+
+get displaycalc(){
+    return this._displaycalc
+}
+
+set displaycalc(value){
+    this._displayCalc = value;
+
+}
+
+get 
+
+}
