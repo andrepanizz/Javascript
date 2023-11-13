5fef353641e53c184c8a24b026a34656c35fa423
Andre Panizza <andredevops7@gmail.com>
2023-11-13 16:33:36 -0300

    update

--- Projeto-javascript/scripts/controller/calcController.js
+++ Projeto-javascript/scripts/controller/calcController.js
@@ -21,6 +21,24 @@ constructor(){
     }
 
     // Metodos get and set:
+    // Criaremos agora o get do DisplayTime:
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value){
+        this._timeEl.innerHTML = value;
+    }
+
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value){
+        return this._dateEl.innerHTML = value;
+    }
+    
+    //
 
     get displayCalc(){
 
