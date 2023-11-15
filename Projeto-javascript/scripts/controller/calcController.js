92ab2e4c28825cc4943164f18c5a1d16605e8979
Andre Panizza <andredevops7@gmail.com>
2023-11-15 15:02:45 -0300

    update

--- Projeto-javascript/scripts/controller/calcController.js
+++ Projeto-javascript/scripts/controller/calcController.js
@@ -1,16 +1,21 @@
 class CalcController {
 
 constructor(){
+   
     this._displayCalcEl = document.querySelector("#display");
     this._dateEl = document.querySelector("#data");
     this._timeEl = document.querySelector("#hora");
     this._currentDate;
     this.initialize();
     
-
     }
 
     initialize(){
+        setInterval(()=>{
+            this.displayDate = this.currentDate.toLocaleDateString("pt-BR");
+            this.displayTime = this.currentDate.toLocaleTimeString("pt-BR");
+        }, 1000);
+        
     
         
     }
