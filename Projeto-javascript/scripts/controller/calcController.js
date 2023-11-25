be25d3a0312941ed7899b0f62a8b9ba348255e6b
Andre Panizza <andredevops7@gmail.com>
2023-11-25 19:24:40 -0300

    update

--- Projeto-javascript/scripts/controller/calcController.js
+++ Projeto-javascript/scripts/controller/calcController.js
@@ -314,7 +314,7 @@ class CalcController {
         })
 
     }
-
+    // setDisplayTime();
     setDisplayDateTime(){
 
         this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
