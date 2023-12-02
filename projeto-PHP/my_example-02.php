75bf5b658bec9e2de5812f431ded53c4c2be4c37
Andre Panizza <andredevops7@gmail.com>
2023-12-01 21:37:55 -0300

    update

--- projeto-PHP/my_example-02.php
+++ projeto-PHP/my_example-02.php
@@ -7,7 +7,7 @@ $my_first = array();
 $my_second = array();
 // adicionando itens ao array();
 array_push($my_first, $fruta_1, $fruta_2, $fruta_3);
-array_push($my_second, "");
+array_push($my_second, $fruta_4, $fruta_5, $fruta_6);
 // Itens para adicionar ao array:
 $fruta_1 = "abacaxi";
 $fruta_2 = "banana";
@@ -19,6 +19,9 @@ $fruta_6 = "limão";
 //Mostrando os valores do meu array:
 var_dump($my_first) . "<br>";
 var_dump($my_second) . "<br>";
+
+// O exemplo acima reproduzira:
+/*     */
 
 
 
