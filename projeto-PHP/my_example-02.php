15af437ba158a50be065176a6b99d2a0b8716178
Andre Panizza <andredevops7@gmail.com>
2023-12-04 18:16:53 -0300

    update

--- projeto-PHP/my_example-02.php
+++ projeto-PHP/my_example-02.php
@@ -2,33 +2,37 @@
 // All about Arrays:
 
 echo "Vamos inicializar com um Array Vazio, para dar exemplos:" . "<br>";
-is_string($separador);
+
 $frutas0 = [];
-$frutas1 = array();
+$nomes1 = array();
 /* @andrepanizz Iniciaremos com array_push que adiciona itens no final do array; */
 array_push($frutas0, "banana","abacaxi","morango","laranja");
-array_push($frutas1, "melancia","uva","manga","maracuja");
+array_push($nomes1, "andre","erica","david","melissa");
 echo '<hr>';
-asort($frutas0);  // DesOrdenada.
-sort($frutas1);   // Ordenada.
 
+asort($frutas0);  // DesOrdenada.
+sort($nomes1);   // Ordenada.
 foreach($frutas0 as $key => $value){
     // Mostrar o que tem dentro do array "frutas0";
-    if($frutas0 === 'string'):
+
+    if(isset($frutas0)):
     echo "Indice" . $key . " <b>nome_da_fruta</b> ". ucfirst($value) . "<br>";
+    
     else:
-        foreach($frutas1 as $key => $val):
-            echo "Indice" . $key . " <b>nome_da_fruta:</b> ".ucwords($val) . "<br>";
+        //Se não existir, remove um item do array $nomes1;
+        echo strtoupper("O array 'nomes1' foi removido o ultima posição! " . "Posição sem a remoção:" . count($nomes1));
+        array_pop($nomes1, "melissa");
+        
+        foreach($nomes1 as $value):
+            echo "Numero de posições:". count($value) . "<br>";
             
         endforeach;
     endif;          
 }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 echo "<hr>";
 
-/* Mostraremos o que tem dentro do array frutas1 */
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 ?>
