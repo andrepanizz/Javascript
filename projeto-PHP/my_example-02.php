4ffade9ffeda06a123522da9e84fae0de20cc6f5
Andre Panizza <andredevops7@gmail.com>
2023-12-04 16:29:56 -0300

    update

--- projeto-PHP/my_example-02.php
+++ projeto-PHP/my_example-02.php
@@ -5,10 +5,14 @@ echo "Vamos inicializar com um Array Vazio, para dar exemplos:" . "<br>";
 
 $frutas0 = [];
 $frutas1 = array();
+/* @andrepanizz Iniciaremos com array_push que adiciona itens no final do array; */
+array_push($frutas0, "Abacaxi","Morango","Banana");
+
 
 // Mostrar o que tem dentro do array();
 echo '<hr>';
 var_dump($frutas0) . "<br>";
+
 var_dump($frutas1) . "<br>";
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
