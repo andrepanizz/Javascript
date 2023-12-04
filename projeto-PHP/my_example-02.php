53ca29103b4329e7a440c0e70df4c946a707ec0a
Andre Panizza <andredevops7@gmail.com>
2023-12-04 14:51:09 -0300

    update

--- projeto-PHP/my_example-02.php
+++ projeto-PHP/my_example-02.php
@@ -3,7 +3,15 @@
 
 echo "Vamos inicializar com um Array Vazio, para dar exemplos:" . "<br>";
 
-$frutas = [];
+$frutas0 = [];
+$frutas1 = array();
+// Mostrar o que tem dentro do array();
+echo $frutas0 . "<br>";
+echo $frutas1 . "<br>";
+echo "<hr>";
+var_dump($frutas0) . "<br>";
+var_dump($frutas1) . "<br>";
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 ?>
