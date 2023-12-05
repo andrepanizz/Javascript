d3f2cc59d54804338bb47483cfcdb8d7d8954eff
Andre Panizza <andredevops7@gmail.com>
2023-12-05 16:20:41 -0300

    update

--- Projeto-manager-users/index.js
+++ Projeto-manager-users/index.js
@@ -1,4 +1,2 @@
 let nome = document.querySelector("#exampleInputName");// eu recupero a informação do id="exampleInputName"
 let nome2 = document.querySelector("#exempleInputName"); // Aqui estou inserindo o valor "OK" dentro do campo;
-
-
