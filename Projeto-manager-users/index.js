3c2663815ee95dc725979922d96a0bdd4d96f002
Andre Panizza <andredevops7@gmail.com>
2023-12-05 16:19:20 -0300

    update

--- Projeto-manager-users/index.js
+++ Projeto-manager-users/index.js
@@ -1,8 +1,2 @@
 let nome = document.querySelector("#exampleInputName");// eu recupero a informação do id="exampleInputName"
 let nome2 = document.querySelector("#exempleInputName"); // Aqui estou inserindo o valor "OK" dentro do campo;
-let elemento = document.getElementById("#exempleInputName");
-let elemento1 = document.getElementsByClassName("#form-group");
-console.log(nome.value = "OI");
-console.log(nome.value);
-console.log(elemento.value);
-console.log(elemento1.value);
