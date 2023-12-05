158de1d3db76dbb6df7dadbea80a5f74413668bc
Andre Panizza <andredevops7@gmail.com>
2023-12-05 16:07:31 -0300

    update

--- Projeto-manager-users/index.js
+++ Projeto-manager-users/index.js
@@ -4,6 +4,8 @@ let elemento = document.getElementById("#exempleInputName");
 let elemento1 = document.getElementsByClassName("#form-group");
 console.log(nome.value = "OI");
 console.log(nome.value);
+console.log(elemento.value);
+
 
 
 
