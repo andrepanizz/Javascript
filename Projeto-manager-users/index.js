7a7934d486aa6c50af5621d39481b694340035fd
Andre Panizza <andredevops7@gmail.com>
2023-12-06 19:18:01 -0300

    update

--- Projeto-manager-users/index.js
+++ Projeto-manager-users/index.js
@@ -1,9 +1,10 @@
 let nome = document.querySelector("#exampleInputName");// eu recupero a informação do id="exampleInputName"
 let nome2 = document.querySelector("#exempleInputName"); // Aqui estou inserindo o valor "OK" dentro do campo;
 /**@param O console.log mostra informações: */
-console.log(nome.value);
-console.log(nome);
-console.log(alert("Aviso!"));
+nome.addEventListener("", e =>{
+    /**@param  */
+});
+
 
 
 
