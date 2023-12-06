8ab6131e63cf0e896e9a36fc842dd78d76b034eb
Andre Panizza <andredevops7@gmail.com>
2023-12-06 19:30:55 -0300

    update

--- Projeto-manager-users/index.js
+++ Projeto-manager-users/index.js
@@ -1,14 +1,5 @@
 let nome = document.querySelector("#exampleInputName");// eu recupero a informação do id="exampleInputName"
 let nome2 = document.querySelector("#exempleInputName"); // Aqui estou inserindo o valor "OK" dentro do campo;
-/**@param O console.log mostra informações: */
-nome.addEventListener("", e =>{
-    /**@param O comando addEventEventListerner(), adiciona um evento passando a variavel
-     * e => como parametro!
-     * literalmente falando, passando 
-    */
-   console.log(e);
-});
-
 
 
 
