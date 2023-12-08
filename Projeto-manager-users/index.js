48425098302af4ba2473cd99e2bc80fbaf9c33fe
Andre Panizza <andredevops7@gmail.com>
2023-12-07 21:53:10 -0300

    update

--- Projeto-manager-users/index.js
+++ Projeto-manager-users/index.js
@@ -1,4 +1,4 @@
 let valor = document.querySelector("#exampleInputName");// eu recupero a informação do id="exampleInputName");
 console.log(valor.value);
 console.log(valor = "OI");
-//
+// continue;
