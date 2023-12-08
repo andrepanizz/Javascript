c039863f169845dfeffcbcb8d13b1a4e37106bd3
Andre Panizza <andredevops7@gmail.com>
2023-12-08 10:11:15 -0300

    update

--- Projeto-manager-users/index.js
+++ Projeto-manager-users/index.js
@@ -1,8 +1,7 @@
 let valor = document.querySelector("#exampleInputName");// eu recupero a informação do id="exampleInputName");
 console.log(valor.value);
 console.log(valor = "OI");
-// continue;
-// this comand console.log make sending for remote tunel.;
+
 
 
 
