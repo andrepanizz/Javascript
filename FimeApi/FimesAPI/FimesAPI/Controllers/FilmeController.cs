311779b1ab780a4cde00230d6677b44835557509
Andre Panizza <andredevops7@gmail.com>
2023-12-09 13:51:18 -0300

    update

---
+++ FimeApi/FimesAPI/FimesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FimesAPI.Controllers
+{
+
+    [ApiController]
+    [Route("controller")]
+    public class FilmeController : ControllerBase
+    {
+
+        private static List<Filme> filmes = new List<Filme>();
+        /* Ela será static porque queremos manter a classe
+         * Queremos ter um metodo pronto para cadastrar no sistema*/
+        public void AdicionaFilme(Filme filme)
+        {
+            /*Precisamos receber alguma informação! vamos receber um parametro  */
+            filmes.Add(filme);
+            /*Nota que precisamos criar nossa lista de filmes, tanto nosso objeto */
+
+        }
+    }
+}
---
+++ FimeApi/FimesAPI/FimesAPI/Models/Filmes.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace FimesAPI.Models
+{
+    public class Filmes
+    {
+        /*- Um filme basicamente tem:
+         -> Um titulo;
+         -> Tempo de Duração;
+         -> Genero*/
+
+        /*Vamos criando nossa propriedade: Titulo, genero, Duração*/
+        public string Titulo { get; set; }
+        public string Genero { get; set; }
+        public int    Duracao { get; set; }
+    }   
+}
