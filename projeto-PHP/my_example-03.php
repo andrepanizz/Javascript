5618cfe6866438928baf154889cdd1aa7bcb1755
Andre Panizza <andredevops7@gmail.com>
2023-12-12 19:14:51 -0300

    update

--- projeto-PHP/my_example-03.php
+++ projeto-PHP/my_example-03.php
@@ -8,6 +8,16 @@ class Carros {
 public string $produto;
 public string $marca;
 public float $preco;
+public string $fabricante;
+public string $vendedor;
+
+
+public function __construct($fabri, $marc)
+{
+    $this->fabricante = $fabri;
+    $this->
+    
+}
 
 public function setProduto($prod){
     $this->produto = $prod;
