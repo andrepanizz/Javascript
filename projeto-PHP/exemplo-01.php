52bd24784dcf91bed2e860d485db5b53f4854611
Andre Panizza <andredevops7@gmail.com>
2023-12-17 16:59:13 -0300

    update

---
+++ projeto-PHP/exemplo-01.php
@@ -0,0 +1,13 @@
+<?php
+$server = "mysql:dbname=dbphp7;";
+$hostname = "host=localhost;";
+$user = "root";
+$password = "root";
+$port = "3307";
+
+$conn = new PDO("$server" . "$hostname", $user, $password);
+
+
+
+
+?>
