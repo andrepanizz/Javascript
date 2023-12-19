f546fd7bf3216c7420a47afbb181732d9f19e73a
Andre Panizza <andredevops7@gmail.com>
2023-12-19 10:19:02 -0300

    update

--- projeto-PHP/exemplo-02.php
+++ projeto-PHP/exemplo-02.php
@@ -1,5 +1,17 @@
 <?php
 
+class register_Users {
+
+    protected string $user;
+    protected string $birth_date;
+    protected string $age;
+    protected string $color_from_the_skin;
+    protected string $admission_from_the_date;
+    
+
+
+
+}
 
 
 
