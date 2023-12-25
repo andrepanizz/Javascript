0df97843f7c47dcd0a6f7439dc3c13341a9f49d7
Andre Panizza <andredevops7@gmail.com>
2023-12-25 19:05:37 -0300

    update

--- projeto-PHP/exemplo-03.php
+++ projeto-PHP/exemplo-03.php
@@ -11,41 +11,8 @@ echo "<center> <b> Connect has been Sucessfully! </b> </center>";
 
 
 
-/* model database:
-CREATE DATABASE register_db;
-USE register_db;
 
-CREATE TABLE table_users(
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(40) NOT NULL,
-endereco VARCHAR(30) NOT NULL,
-numero INT(4) NOT NULL,
-dtcadastro TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-birth_date VARCHAR(6) NOT NULL,
-idade INT(2) NOT NULL
-);
-
-SELECT * FROM table_users;
-
-INSERT INTO table_users (nome, endereco, numero, birth_date, idade) VALUES ('Andre','Rua Paes de Barros', 133,27-02-1998, 50);
-INSERT INTO table_users (nome, endereco, numero) VALUES ('Erica','Rua Enersto Fogo', 200);
-DELETE from table_users WHERE id = 1;
-
-TRUNCATE TABLE table_users;
-
-DROP DATABASE register_db;
-
-
-# ATER TABLE  table_users ADD nome  VARCHAR(30);
-# ALTER TABLE table_users ADD email VARCHAR(20);
-# ALTER TABLE table_users ADD cep   VARCHAR(9);
-# ALTER TABLE table_users ADD cidade VARCHAR(15);
-# ALTER TABLE table_users ADD endereco VARCHAR(40);
+/*
 
 */
 
-
-
-
-
-?>
