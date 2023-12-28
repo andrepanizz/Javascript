7218a937185870036c2ebf41ca8903a70d0f98b5
Andre Panizza <andredevops7@gmail.com>
2023-12-28 14:57:00 -0300

    update

--- projeto-PHP/config/connection_factory.php
+++ projeto-PHP/config/connection_factory.php
@@ -2,6 +2,16 @@
 // Aqui será criada uma classe mais elaborada para efetiva:
 require_once("../config/connection.php");
 
+class connection_factory {
+    // criaremos aqui uma função que efetivamente inicializa a conexão;
+    public static function createConnection() {
+        $connection =  new Connection(
+            // aqui devemos passar as conexões:
+
+        )
+
+    }
+}
 
 
 
