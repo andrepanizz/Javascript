2d26ff095f353b0a6306dd4beced4ee1932e3a43
Andre Panizza <andredevops7@gmail.com>
2023-12-28 15:00:18 -0300

    update

--- projeto-PHP/config/connection_factory.php
+++ projeto-PHP/config/connection_factory.php
@@ -3,13 +3,19 @@
 require_once("../config/connection.php");
 
 class connection_factory {
+
     // criaremos aqui uma função que efetivamente inicializa a conexão;
     public static function createConnection() {
         $connection =  new Connection(
             // aqui devemos passar as conexões:
+            "localhost",
+            "db1",
+            "3306",
+            "root",
+            ""
 
-        )
-
+        );
+        // Aqui efeti
     }
 }
 
