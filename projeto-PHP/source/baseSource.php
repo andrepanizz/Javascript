760d16892b2fe2380c03af2afbdd6a2ba24c53a9
Andre Panizza <andredevops7@gmail.com>
2023-12-30 15:46:55 -0300

    update

--- projeto-PHP/source/baseSource.php
+++ projeto-PHP/source/baseSource.php
@@ -1,19 +1,7 @@
 <?php
 require_once("../config/connection_factory.php");
 
-/*
- Agora iremos criar um classe abstrata:
-Nota sobre as classes e functions abstratas:
--> A classe abstrata ou Abstract class e uma classe que 
-não pode ser instanciada, mas poderá ser herdada seus
-atributos se tiverem sido definidos como: 
--> public
--> protected
 
-somente os metodos e os atributos podem ser herdados e acessados
-podemos definir: function abstract ou abstract class.
-iremos iniciar com esta function para começarmos 
-*/
 
 
 ?>
