494aed148d4707744e568c65b1480b4d42df9330
Andre Panizza <andredevops7@gmail.com>
2023-12-31 11:55:01 -0300

    update

--- projeto-PHP/training/exemplo-01.php
+++ projeto-PHP/training/exemplo-01.php
@@ -4,7 +4,11 @@
 #arrays basicos:
 
 $array = array();
-// a function array_push adiciona itens ao final do array
+// a function array_push adiciona itens ao final do array;
+array_push();
+
+print_r($array) . "<br>";
+
 
 
 
