d28737a3f57ae1fa8d8479689288fc65e0158d3a
Andre Panizza <andredevops7@gmail.com>
2023-12-31 11:55:40 -0300

    update

--- projeto-PHP/training/exemplo-01.php
+++ projeto-PHP/training/exemplo-01.php
@@ -5,10 +5,10 @@
 
 $array = array();
 // a function array_push adiciona itens ao final do array;
-array_push();
+array_push($array, "laranja", "acerola", "manga");
 
 print_r($array) . "<br>";
-
+// A function 
 
 
 
